04a422c3f0b49d28cd9a8bd4804419a564594403
Rohan Shah <rohanshah945@gmail.com>
2020-07-29 15:15:42 +0530

    Updates

---
+++ precache-manifest.fadc541e134309914812198b7ef6c988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d13065093b77cfeb0d180b139d4da13",
+    "url": "/product_catelogue_demo/index.html"
+  },
+  {
+    "revision": "eb681fd540eb7afc2019",
+    "url": "/product_catelogue_demo/static/css/main.9237605b.chunk.css"
+  },
+  {
+    "revision": "a583288579d64a3c984b",
+    "url": "/product_catelogue_demo/static/js/2.79bb1c6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/product_catelogue_demo/static/js/2.79bb1c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb681fd540eb7afc2019",
+    "url": "/product_catelogue_demo/static/js/main.7b8d7afb.chunk.js"
+  },
+  {
+    "revision": "287f8bcf56ac84cd81c0",
+    "url": "/product_catelogue_demo/static/js/runtime-main.3196f575.js"
+  }
+]);
